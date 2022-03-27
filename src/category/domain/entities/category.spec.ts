b021d4147b99c04d310fea854fada8d93a32d8a9
wilfoz <wil_foz@yahoo.com.br>
2022-03-27 13:08:19 -0300

    build: entity category and test

--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -1,6 +1,6 @@
 import {Category} from './category';
 
-describe('Category Tests', () => {
+describe('Category Unit Tests', () => {
    test('constructor of category', () => {
        const category = new Category('Movie');
        expect(category.name).toBe('Movie');
--- src/category/domain/entities/category.ts
+++ src/category/domain/entities/category.ts
@@ -1,3 +1,26 @@
+export type CategoryProperties = {
+    name: string;
+    description?: string;
+    is_active?: boolean;
+    created_at?: Date;
+}
+
 export class Category {
-    constructor(public name: string) {}
+    constructor(readonly props: CategoryProperties) {}
+
+    get name(): string {
+        return this.props.name;
+    }
+
+    get description(): string | undefined {
+        return this.props.description;
+    }
+
+    get isActive(): boolean | undefined {
+        return this.props.is_active;
+    }
+
+    get createdAt(): Date | undefined {
+        return this.props.created_at;
+    }
 }
